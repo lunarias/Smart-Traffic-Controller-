96542a88cd6fb343a0aea2dc24e739192c885558
Colin Au Yeung <daisuke.moto403@gmail.com>
2019-02-16 17:20:10 -0700

    controller and car updates

--- Smart-Traffic-Controller/src/simulator/Controller.java
+++ Smart-Traffic-Controller/src/simulator/Controller.java
@@ -1,5 +1,30 @@
 package simulator;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Controller{
-    
+
+    private int grid_height = 8;
+    private int grid_width = 8;
+
+    private ArrayList<ArrayList<Integer>> cars = new ArrayList<>();
+    private ArrayList<ArrayList<Intersection>> intersections = new ArrayList<>();
+    private ArrayList<ArrayList<Road>> roads = new ArrayList<>();
+
+
+    public Controller(){
+        for(int i=0; i<grid_width; i++ ){
+            cars.add(new ArrayList<>());
+            intersections.add(new ArrayList<>());
+            roads.add(new ArrayList<>());
+            for(int j = 0; j<grid_height; j++){
+                cars.get(i).add(0);
+                intersections.get(i).add(new Intersection());
+                roads.get(i).add(new Road());
+            }
+        }
+    }
+
+
 }
