e11dc138677adaccc4633961bf2161de6171de0c
brandon.lu1 <brandon.lu1@ucalgary.ca>
2019-02-16 17:22:04 -0700

    Working on intersection
    Pulling new content

--- Smart-Traffic-Controller/src/simulator/Intersection.java
+++ Smart-Traffic-Controller/src/simulator/Intersection.java
@@ -2,4 +2,24 @@ package simulator;
 
 public class Intersection {
     
+    public Intersection() {
+    
+    }
+    
+    private boolean ns_status;
+    private boolean ew_status;
+    
+    public boolean set_nw_status(Boolean status) {
+        this.ns_status = status;
+    }
+    
+    
+    public boolean is_nw_active() {
+        return ns_status;
+    }
+    
+    public boolean is_ew_active() {
+        return ew_status;
+    }
+    
 }
