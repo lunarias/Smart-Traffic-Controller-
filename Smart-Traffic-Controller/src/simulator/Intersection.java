4ce192c5670ac66f894b342f0ae46aa0c89f75a6
brandon.lu1 <brandon.lu1@ucalgary.ca>
2019-02-16 17:41:57 -0700

    Updated intersection
    Fixed methods

--- Smart-Traffic-Controller/src/simulator/Intersection.java
+++ Smart-Traffic-Controller/src/simulator/Intersection.java
@@ -9,10 +9,13 @@ public class Intersection {
     private boolean ns_status;
     private boolean ew_status;
     
-    public void set_nw_status(Boolean status) {
+    public void set_ns_status(Boolean status) {
         this.ns_status = status;
     }
     
+    public void set_ew_status(Boolean status) {
+        this.ew_status = status;
+    }
     
     public boolean is_nw_active() {
         return ns_status;
