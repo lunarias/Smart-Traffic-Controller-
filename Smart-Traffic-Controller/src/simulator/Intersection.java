975e4056bb0b69262f7762431ea2a9d38e3eb154
brandon.lu1 <brandon.lu1@ucalgary.ca>
2019-02-16 22:34:28 -0700

    Pull new content

--- Smart-Traffic-Controller/src/simulator/Intersection.java
+++ Smart-Traffic-Controller/src/simulator/Intersection.java
@@ -4,7 +4,14 @@ import java.util.List;
 public class Intersection {
     
     private int carsPassed;
+    
     private List directions_active;
+    
+    
+    // Priority for turns, weighted - currently set to 1/3 chance for any turn
+    private double left_priority = 1;
+    private double straight_priority = 1;
+    private double right_priority = 1;
     
     
     public Intersection(List directions) {
